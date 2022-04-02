a1a17caf44df87a47ab2f726ec8a2388300d056a
darkpc1412 <102814997+darkpc1412@users.noreply.github.com>
2022-04-02 09:53:22 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -835,7 +835,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)       
         await query.message.edit_text(
-            text="𝙾𝙿𝚄𝚂-𝚃𝙴𝙲𝙷𝚉"
+            text="�𝐌𝐎𝐕𝐈𝐄 𝐂𝐋𝐔𝐁 📺🎥"
         )
         await query.message.edit_text(                     
             text=script.HELP_TXT.format(query.from_user.mention),
@@ -851,7 +851,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await query.message.edit_text(
-            text="𝙾𝙿𝚄𝚂-𝚃𝙴𝙲𝙷𝚉"
+            text="�𝐌𝐎𝐕𝐈𝐄 𝐂𝐋𝐔𝐁 📺🎥"
         )
         await query.message.edit_text(
             text=script.ABOUT_TXT,
